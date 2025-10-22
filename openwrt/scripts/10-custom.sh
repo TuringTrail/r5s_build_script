757a300cb2402b4bccb5d25babc01267b66a31da
Turing <turing@xusw.com>
2025-10-22 12:40:20 +0800

    feat: switch from passwall to ssr-plus and add lucky package in OpenWrt config

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # 自定义脚本
-git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/new/luci-app-easytier
+git clone  https://github.com/EasyTier/luci-app-easytier package/new/luci-app-easytier
+git clone  https://github.com/gdy666/luci-app-lucky package/new/lucky
+git clone  https://github.com/fw876/helloworld package/new/helloworld
